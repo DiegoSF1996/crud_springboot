f526accbe15fde22a35b71d511a559d1e82d451d
Diego.Figueiredo <diegosf.dsf@gmail.com>
2024-05-31 12:04:16 -0300

    feature: Add relacionamento ClientProduct com Client

--- src/main/java/com/example/produto/produto_app/models/ClientProductModel.java
+++ src/main/java/com/example/produto/produto_app/models/ClientProductModel.java
@@ -3,11 +3,13 @@ package com.example.produto.produto_app.models;
 import java.io.Serializable;
 import java.util.UUID;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,9 +20,38 @@ public class ClientProductModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    
+    @Column(name = "idClient")
     private UUID idClient;
+    
+    @Column(name = "idProduct")
     private UUID idProduct;
     
+    @OneToOne
+    @JoinColumn(name = "idClient", referencedColumnName = "idClient", insertable = false, updatable = false)
+    private ClientModel client;
+ 
+
+    @OneToOne
+    @JoinColumn(name = "idProduct",referencedColumnName = "idProduct", insertable = false, updatable = false)
+    private ProductModel product;
+
+
+    public ClientModel getClient() {
+        return client;
+    }
+
+    public void setClient(ClientModel client) {
+        this.client = client;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
 
     public UUID getIdClient() {
         return idClient;
